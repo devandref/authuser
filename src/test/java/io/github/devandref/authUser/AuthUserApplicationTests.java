cfbf6475ef7f788de2509ace1ecd9732483aea89
devandref <yoandredepaula@gmail.com>
2024-03-11 07:24:16 -0300

    first commit

---
+++ src/test/java/io/github/devandref/authUser/AuthUserApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devandref.authUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
