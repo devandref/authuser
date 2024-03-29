8f99904e223bea4a1ea78785e4872d365d63f321
devandref <yoandredepaula@gmail.com>
2024-03-28 21:20:05 -0300

    include: métodos para listar, deletar via http request

--- src/main/java/io/github/devandref/authUser/service/UserService.java
+++ src/main/java/io/github/devandref/authUser/service/UserService.java
@@ -1,7 +1,14 @@
 package io.github.devandref.authUser.service;
 
+import io.github.devandref.authUser.model.UserModel;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface UserService {
 
-
-
+    List<UserModel> findAll();
+    Optional<UserModel> findById(UUID userId);
+    void delete(UserModel userModel);
 }
--- src/main/java/io/github/devandref/authUser/service/impl/UserServiceImpl.java
+++ src/main/java/io/github/devandref/authUser/service/impl/UserServiceImpl.java
@@ -1,9 +1,14 @@
 package io.github.devandref.authUser.service.impl;
 
+import io.github.devandref.authUser.model.UserModel;
 import io.github.devandref.authUser.repositories.UserRepository;
 import io.github.devandref.authUser.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -11,6 +16,19 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Override
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
 
+    @Override
+    public Optional<UserModel> findById(UUID userId) {
+        return userRepository.findById(userId);
+    }
+
+    @Override
+    public void delete(UserModel userModel) {
+        userRepository.delete(userModel);
+    }
 
 }
